f690c8c2935c43967dd84fa51429aa8e39bd31ad
16115051 <himanshuamd1@gmail.com>
2019-04-11 15:18:34 +0530

    update

--- Virtual_Labs/settings.py
+++ Virtual_Labs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r(ij9t9ve5ekj7s0k2_=n8!^dvd2$&r@!0e=&s##*kb#6!=i#z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
