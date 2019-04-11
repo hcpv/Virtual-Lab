f93718d8559aad7d8008d94ed2d6cbfea26e0baf
16115051 <himanshuamd1@gmail.com>
2019-04-11 16:21:43 +0530

    update

--- Virtual_Labs/settings.py
+++ Virtual_Labs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r(ij9t9ve5ekj7s0k2_=n8!^dvd2$&r@!0e=&s##*kb#6!=i#z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
